38c5fb2de5835f49adeb44d2da951e4439f8ec3e
GabGua <82704247+GabGua@users.noreply.github.com>
2021-06-20 19:24:31 +0200

    .

--- terraform/correccion-vars.tf
+++ terraform/correccion-vars.tf
@@ -13,7 +13,7 @@ variable "storage_account" {
 variable "public_key_path" {
   type = string
   description = "Ruta para la clave pública de acceso a las instancias"
-  default = "C:/Users/guarino/Documents/GitHub/CP2/.ssh/id_rsa" # o la ruta correspondiente
+  default = "C:/Users/guarino/Documents/GitHub/CP2/.ssh/id_rsa.pub" # o la ruta correspondiente
 }
 
 variable "ssh_user" {
